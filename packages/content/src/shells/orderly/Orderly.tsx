109128032a29bc35761634410f993b819e22d094
orderlyapps <orderlyapps@gmail.com>
2025-03-09 13:18:03 +1100

    init

--- packages/content/src/shells/orderly/Orderly.tsx
+++ packages/content/src/shells/orderly/Orderly.tsx
@@ -1,13 +1,10 @@
-// import { ORDERLY_ROUTES } from "@workspace/pages/ORDERLY_ROUTES";
-// import { ORDERLY_ROUTES } from "@workspace/content/orderly";
-
 import { ReactQueryProvider } from "@amodeo/data/react-query-provider";
 import { TabsApp } from "@amodeo/ui/ionic/tabs-app/TabsApp";
 import { UpdateNotification } from "@amodeo/ui/ionic/update-notification/UpdateNotification";
+import { ORDERLY_ROUTES } from "./routes.js";
 
 import "@amodeo/ui/ionic/tabs-app/utils/ionicInit";
 import "@amodeo/util/css/orderly";
-import { ORDERLY_ROUTES } from "./routes.js";
 
 export function Orderly({ useRegisterSW }: { useRegisterSW?: any }) {
   return (
