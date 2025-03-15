8e5716c9e156759a4f8464ae01cc4553b769c7d9
orderlyapps <orderlyapps@gmail.com>
2025-03-16 08:43:53 +1100

    Update testing app and windsurfrules

--- apps/testing/src/App.tsx
+++ apps/testing/src/App.tsx
@@ -1,15 +1,7 @@
-import TemplatePDF from "../../../packages/feature/src/pdf/util/TemplatePDF";
 import "@amodeo/util/css/testing";
 
 function App() {
-  return (
-    <div
-      // className="full centered"
-      style={{ backgroundColor: "lightgreen", height: "95vh", width: "95%" }}
-    >
-      <TemplatePDF.Render />
-    </div>
-  );
+  return <div>Testing</div>;
 }
 
 export default App;
