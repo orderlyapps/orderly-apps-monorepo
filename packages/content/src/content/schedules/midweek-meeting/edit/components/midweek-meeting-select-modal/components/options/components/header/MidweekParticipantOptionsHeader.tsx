c00899365650fa74e6ab4579b4a4c7a3783e7b26
orderlyapps <orderlyapps@gmail.com>
2025-05-08 13:48:47 +1000

    feat: add assignment status indicators and average weeks display in participant header

--- packages/content/src/content/schedules/midweek-meeting/edit/components/midweek-meeting-select-modal/components/options/components/header/MidweekParticipantOptionsHeader.tsx
+++ packages/content/src/content/schedules/midweek-meeting/edit/components/midweek-meeting-select-modal/components/options/components/header/MidweekParticipantOptionsHeader.tsx
@@ -1,6 +1,7 @@
 import { IonText } from "@ionic/react";
 import { filterValues } from "../../../../hooks/use-midweek-assignments-form-state/use-midweek-assignments-form-state.js";
 import { ParticipantType } from "../../../../hooks/use-midweek-participants-list/use-midweek-participants-list.js";
+import { properAssignments } from "../../../../hooks/use-midweek-participants-list/helper/get-extra-data/helper/assignmentData.js";
 
 export function MidweekParticipantOptionsHeader({
   participant,
@@ -9,15 +10,28 @@ export function MidweekParticipantOptionsHeader({
   participant: ParticipantType;
   sortValue: keyof typeof filterValues;
 }) {
+  const hasAssignment = participant.currentAssignments.some((assignments) => {
+    return properAssignments.includes(assignments.assignment);
+  });
+
   return (
     <>
       <IonText
         slot="start"
-        color={participant.isCurrentAssignee ? "primary" : ""}
+        color={
+          participant.isCurrentAssignee
+            ? "primary"
+            : hasAssignment
+              ? "medium"
+              : ""
+        }
       >
         <strong>
           {participant.first_name} {participant.last_name}
         </strong>
+        {participant.averageWeeksBetweenAssignments
+          ? ` (avg: ${participant.averageWeeksBetweenAssignments})`
+          : ""}
       </IonText>
 
       <IonText slot="end" className="ion-margin-end">
