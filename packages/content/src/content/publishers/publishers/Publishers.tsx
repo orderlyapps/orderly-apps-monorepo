42653f20309b8331f425b32822a7b2627500d84c
orderlyapps <orderlyapps@gmail.com>
2025-05-16 06:09:44 +1000

    style: update card navigation colors and remove alpha version check

--- packages/content/src/content/publishers/publishers/Publishers.tsx
+++ packages/content/src/content/publishers/publishers/Publishers.tsx
@@ -2,25 +2,20 @@ import { orderlyPath } from "#shells/orderly/routes.js";
 import { CardNav } from "@amodeo/ui/ionic/card-nav/CardNav";
 import { congregationIcon } from "@amodeo/ui/ionic/icons/congregation";
 import { visitingSpeakerIcon } from "@amodeo/ui/ionic/icons/visiting-speakers";
-import { IonItem, IonList, IonText } from "@ionic/react";
+import { IonList } from "@ionic/react";
 
 export const Publishers = () => (
   <IonList>
-    {IS_ALPHA_VERSION && (
-      <IonItem>
-        <IonText>Alpha</IonText>
-      </IonItem>
-    )}
     <CardNav
       label="Congregation"
       path={orderlyPath("congregation")}
-      color="medium"
+      color="jw_slate"
       icon={congregationIcon}
     />
     <CardNav
       label="Visiting Speakers"
       path={orderlyPath("visiting_speakers")}
-      color="medium"
+      color="jw_slate"
       icon={visitingSpeakerIcon}
     />
   </IonList>
