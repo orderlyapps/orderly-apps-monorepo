2a70fd699cb995ae335462602e0805502b34aff7
orderlyapps <orderlyapps@gmail.com>
2025-06-29 16:47:16 +1000

    update: refresh cleaning schedule data and remove unused imports

--- packages/content/src/content/schedules/cleaning/Cleaning.tsx
+++ packages/content/src/content/schedules/cleaning/Cleaning.tsx
@@ -1,24 +1,15 @@
 import { ModalProps } from "@amodeo/ui/ionic/use-card-modal/useCardModal";
 import {
   IonCard,
-  IonCardHeader,
-  IonCardTitle,
   IonCardContent,
   IonItem,
   IonLabel,
   IonList,
   IonListHeader,
-  IonNote,
-  IonIcon,
   IonGrid,
   IonRow,
   IonCol,
 } from "@ionic/react";
-import {
-  calendarOutline,
-  peopleOutline,
-  sparklesOutline,
-} from "ionicons/icons";
 
 type CleaningScheduleItem = {
   week: string;
@@ -28,23 +19,48 @@ type CleaningScheduleItem = {
 
 const cleaningScheduleData: CleaningScheduleItem[] = [
   {
-    week: "Jun 9-15",
-    sanitation: "Group 2",
+    week: "Jun 30 - Jul 6",
+    sanitation: "Group 5",
     cleaning: "N/A",
   },
   {
-    week: "Jun 16-22",
+    week: "Jul 7-13",
+    sanitation: "Group 6",
+    cleaning: "Group 1",
+  },
+  {
+    week: "Jul 14-20",
+    sanitation: "Group 1",
+    cleaning: "Group 2",
+  },
+  {
+    week: "Jul 21-27",
+    sanitation: "Convention",
+    cleaning: "",
+  },
+  {
+    week: "Jul 28-Aug 3",
+    sanitation: "Group 2",
+    cleaning: "Group 3",
+  },
+  {
+    week: "Aug 4-10",
     sanitation: "Group 3",
     cleaning: "N/A",
   },
   {
-    week: "Jun 23-29",
+    week: "Aug 11-17",
     sanitation: "Group 4",
     cleaning: "N/A",
   },
   {
-    week: "Jun 30 - Jul 6",
+    week: "Aug 18-24",
     sanitation: "Group 5",
+    cleaning: "N/A",
+  },
+  {
+    week: "Aug 25-Aug 31",
+    sanitation: "Group 6",
     cleaning: "N/A",
   },
 ];
