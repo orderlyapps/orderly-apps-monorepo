e6a2e8d9e145f0b4972dab43c56313f8989258ac
orderlyapps <orderlyapps@gmail.com>
2025-06-29 20:09:47 +1000

    fix: remove default Kingdom Hall location from field service schedule

--- packages/content/src/content/schedules/field-service/FieldService.tsx
+++ packages/content/src/content/schedules/field-service/FieldService.tsx
@@ -67,7 +67,7 @@ const scheduleData: ScheduleDay[] = [
     details: [
       {
         time: "9:15 AM",
-        location: "Kingdom Hall",
+        location: "",
         conductor: "Group Overseers",
       },
     ],
